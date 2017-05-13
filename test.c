99ddc5156685e14ccb3f6076d041b81e8a8ab3b3
marianouria <marianouriavalencia@gmail.com>
2017-05-13 01:34:32 -0500

    ver130517

--- test.c
+++ test.c
@@ -28,8 +28,144 @@ typedef double vector[3];
 typedef vector triplet[3];
 typedef double matrix[3][3];
 
-int main (){
-	
-	return 0;
+/* storage locations */
+struct Data {
+        triplet xi;             /* ground state */
+        double kac, kb;         /* half-rigidities */
+} *set;
+triplet *xx;
+vector *rr;
+double *erg;
 
+/* all defaults */
+int NS = 0, Lk = 0;
+int FREQ = 100000;
+double bps = 3.4829;            /* gives density */
+double diam2 = 0.0;
+double rdata[3] = { 0.1, 0.1, 0.3 };
+const double relaxer[3] = { 4.0, 4.0, 2.0 };
+
+double bound[3];
+double condit[3];
+double current[3];
+
+void eulerset(triplet x, double alpha, double beta, double gamma)
+{
+        double sa, ca, sb, cb, sc, cc;
+
+        sin_cos(sa, ca, alpha);
+        sin_cos(sb, cb, beta);
+        sin_cos(sc, cc, gamma);
+
+        x[0][0] = ca * cc - sa * cb * sc;
+        x[0][1] = sa * cc + ca * cb * sc;
+        x[0][2] = sb * sc;
+
+        x[1][0] = -ca * sc - sa * cb * cc;
+        x[1][1] = -sa * sc + ca * cb * cc;
+        x[1][2] = sb * cc;
+
+        x[2][0] = sa * sb;
+        x[2][1] = -ca * sb;
+        x[2][2] = cb;
 }
+
+
+void allocmemory()
+{
+        xx = (triplet *) malloc(sizeof(triplet) * (NS + 1));
+        rr = (vector *) malloc(sizeof(vector) * (NS + 1));
+        erg = (double *) malloc(sizeof(double) * (NS + 1));
+        set = (struct Data *) malloc(sizeof(struct Data) * (NS + 1));
+
+        if (xx == NULL || rr == NULL || erg == NULL || set == NULL) {
+                printf("Not enough memory\n");
+                exit(EXIT_FAILURE);
+        }
+}
+
+
+void readdata()
+{
+        int i = 1, numbr;
+        char c[83];
+        double twist_eq = 0., arc_eq = 0.;
+        double alpha, beta, gamma, sac, sb;
+
+        while (fgets(c, sizeof(c), stdin) != NULL) {
+                if (c[0] == '#' || c[0] == '\n')
+                        continue;
+
+                if (NS == 0 && sscanf(c, "Number of segments %d", &NS) == 1) {
+                      	allocmemory();
+                        continue;
+                }
+
+                if (sscanf(c, "Segments: number alpha beta gamma dac db %*d")
+                    == EOF) {
+                        i = 1;
+                        twist_eq = 0.;
+                        arc_eq = 0.;
+                        continue;
+                }
+
+                if (sscanf(c, "%d %lf %lf %lf %lf %lf",
+                           &numbr, &alpha, &beta, &gamma, &sac, &sb) == 6) {
+                        for (; numbr > 0 && i <= NS; numbr--, i++) {
+                                eulerset(set[i].xi, alpha, beta, gamma);
+                                set[i].kac = 0.5 / (sac * sac);
+                                set[i].kb = 0.5 / (sb * sb);
+                                twist_eq += alpha + gamma;
+                                arc_eq += beta;
+                        }
+                        continue;
+                }
+		
+		if (sscanf(c, "Diameter-to-length ratio %lf", &diam2) == 1) {
+                        diam2 *= diam2;
+                        continue;
+                }
+
+                if (sscanf(c, "Linking number %d", &Lk) == 1)
+                        continue;
+
+                if (sscanf(c, "Segment length %lf", &bps) == 1)
+                        continue;
+
+                if (sscanf(c, "Conformation restriction: tw bd r %*d") == EOF) {
+                        fscanf(stdin, "%lf %lf %lf",
+                               rdata, rdata + 1, rdata + 2);
+                        rdata[2] /= bps;        /* to segment length */
+                        continue;
+                }
+
+                if (sscanf(c, "Number of movements %d", &FREQ) == 1)
+                        continue;
+        }
+
+        NS = i - 1;
+
+        /* report what was read */
+        printf("Segments = %d   Arc = %g   Tw = %g   Lk = %d   SqDiam = %g\n",
+               NS, arc_eq / (2 * M_PI), twist_eq / (2 * M_PI), Lk, diam2);
+}
+
+
+int main(int argc, char *argv[])
+{
+        int i, exps = 9;
+        double wolume, v, mv = 0.0, ev = 0.0;
+
+        srand((unsigned) time(NULL));
+
+        if (argc > 1)
+                if (freopen(argv[1], "rt", stdin) == NULL) {
+                        printf(VER "Can't open %s\nUsage: %s [infile]\n",
+                               argv[1], argv[0]);
+                        return EXIT_FAILURE;
+                }
+        readdata();
+
+return EXIT_SUCCESS;
+}
+
